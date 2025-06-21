d432a14a8ace721b6701f03e2590897f13f85d95
Evelyn Moore <91475934+WillMoore66@users.noreply.github.com>
2025-06-21 21:15:55 +0100

    added tweening to godot buttons
    next step is the tweened intro in godot

--- Godot Project/ui-tech-art/ButtonMouseOverTween.gd
+++ Godot Project/ui-tech-art/ButtonMouseOverTween.gd
@@ -1,18 +1,24 @@
 extends Button
 
-@onready var original_position := position
-var tween: SceneTreeTween
+@export var distanceToMove: float = 20.0;
+@export var duration: float = 0.1;
+var original_position: Vector2
+var tween
+
+func _ready():
+	original_position = position
+	print(original_position)
 
 func _on_mouse_entered():
-	if tween: tween.kill()  # Cancel any running tweens
+	if tween: tween.kill()
 	tween = get_tree().create_tween()
-	tween.tween_property(self, "position", original_position + Vector2(10, 0), 0.2)\
+	tween.tween_property(self, "position", original_position + Vector2(distanceToMove, 0), 0.2)\
 		.set_trans(Tween.TRANS_SINE)\
-		.set_ease(Tween.EASE_OUT)
+		.set_ease(Tween.EASE_IN_OUT)
 
 func _on_mouse_exited():
 	if tween: tween.kill()
 	tween = get_tree().create_tween()
-	tween.tween_property(self, "position", original_position, 0.2)\
+	tween.tween_property(self, "position", original_position, duration)\
 		.set_trans(Tween.TRANS_SINE)\
-		.set_ease(Tween.EASE_OUT)
+		.set_ease(Tween.EASE_IN_OUT)
