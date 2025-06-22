0554c4c96a2ccc4a1cd7dbf45d8b6723130ea019
Evelyn Moore <91475934+WillMoore66@users.noreply.github.com>
2025-06-22 12:23:12 +0100

    add fire fade effect to unity project

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
@@ -8,12 +9,27 @@ using DG.Tweening;
 public class MainMenu : MonoBehaviour
 {
     [SerializeField] List<RectTransform> buttons = new List<RectTransform>();
+    [SerializeField] List<Material> buttonMaterials = new List<Material>();
 
     private Dictionary<RectTransform, Vector2> originalPositions = new Dictionary<RectTransform, Vector2>();
     private Dictionary<RectTransform, Tween> activeTweens = new Dictionary<RectTransform, Tween>();
+    private Dictionary<RectTransform, Material> buttonMaterialMap = new Dictionary<RectTransform, Material>();
 
     void Awake()
     {
+        if (buttonMaterials.Count != buttons.Count)
+        {
+            Debug.LogError("Button materials count doesn't match buttons count!");
+            return;
+        }
+
+
+        for (int i = 0; i < buttons.Count; i++)
+        {
+            buttonMaterialMap[buttons[i]] = buttonMaterials[i];
+            buttonMaterials[i].SetFloat("_Fade_Right_Amount", 0f);
+        }
+
         foreach (RectTransform rect in buttons)
         {
             Vector2 startPos = rect.anchoredPosition;
@@ -21,8 +37,7 @@ public class MainMenu : MonoBehaviour
             rect.anchoredPosition = startPos;
 
             originalPositions[rect] = new Vector2(-350f, startPos.y);
-
-            // Add event handlers
+            
             AddHoverEvents(rect);
         }
 
@@ -43,14 +58,12 @@ public class MainMenu : MonoBehaviour
         EventTrigger trigger = rect.gameObject.GetComponent<EventTrigger>();
         if (trigger == null)
             trigger = rect.gameObject.AddComponent<EventTrigger>();
-
-        // PointerEnter
+        
         EventTrigger.Entry entryEnter = new EventTrigger.Entry();
         entryEnter.eventID = EventTriggerType.PointerEnter;
         entryEnter.callback.AddListener((data) => OnHoverEnter(rect));
         trigger.triggers.Add(entryEnter);
-
-        // PointerExit
+        
         EventTrigger.Entry entryExit = new EventTrigger.Entry();
         entryExit.eventID = EventTriggerType.PointerExit;
         entryExit.callback.AddListener((data) => OnHoverExit(rect));
@@ -74,24 +87,46 @@ public class MainMenu : MonoBehaviour
         Vector2 targetPos = originalPositions[rect];
         activeTweens[rect] = rect.DOAnchorPos(targetPos, 0.25f).SetEase(Ease.OutQuad);
     }
-    
+
+    void AnimateButtonEffect(RectTransform button)
+    {
+        if (buttonMaterialMap.TryGetValue(button, out Material mat))
+        {
+            mat.SetFloat("_Fade_Right_Amount", 0f);
+            
+            DOTween.To(() => mat.GetFloat("_Fade_Right_Amount"), 
+                     x => mat.SetFloat("_Fade_Right_Amount", x), 
+                     5f, 6f)
+                  .SetEase(Ease.OutQuad);
+        }
+    }
+
+    private void OnDestroy()
+    {
+        for (int i = 0; i < buttons.Count; i++)
+        {
+            buttonMaterialMap[buttons[i]] = buttonMaterials[i];
+            buttonMaterials[i].SetFloat("_Fade_Right_Amount", 1.0f);
+        }
+    }
+
     public void Play()
     {
-        
+        if (buttons.Count > 0) AnimateButtonEffect(buttons[0]);
     }
     
     public void Settings()
     {
-        
+        if (buttons.Count > 1) AnimateButtonEffect(buttons[1]);
     }
     
     public void Credits()
     {
-        
+        if (buttons.Count > 2) AnimateButtonEffect(buttons[2]);
     }
     
     public void Exit()
     {
-        
+        if (buttons.Count > 3) AnimateButtonEffect(buttons[3]);
     }
 }
--- Godot Project/ui-tech-art/ButtonMouseOverTween.gd
+++ Godot Project/ui-tech-art/ButtonMouseOverTween.gd
@@ -2,17 +2,25 @@ extends Button
 
 @export var distanceToMove: float = 20.0;
 @export var duration: float = 0.1;
+@export var distToTeleport: float = 300;
+@export var waitBeforeInitMove: float = 0.0;
+@export var initMoveDuration= 0.5;
 var original_position: Vector2
 var tween
 
 func _ready():
 	original_position = position
-	print(original_position)
+	position.x -= distToTeleport;
+	tween = get_tree().create_tween()
+	tween.tween_interval(waitBeforeInitMove)
+	tween.tween_property(self, "position", original_position, initMoveDuration)\
+		.set_trans(Tween.TRANS_SINE)\
+		.set_ease(Tween.EASE_IN_OUT)
 
 func _on_mouse_entered():
 	if tween: tween.kill()
 	tween = get_tree().create_tween()
-	tween.tween_property(self, "position", original_position + Vector2(distanceToMove, 0), 0.2)\
+	tween.tween_property(self, "position", original_position + Vector2(distanceToMove, 0), duration)\
 		.set_trans(Tween.TRANS_SINE)\
 		.set_ease(Tween.EASE_IN_OUT)
 
